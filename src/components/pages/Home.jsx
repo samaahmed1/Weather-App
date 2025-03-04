39840e9e5b264b27c124fa80d6d4c284ce8d063e
samaahmed1 <sama34777@gmail.com>
2025-03-04 20:52:22 +0200

    Refactor: Move background styling to global styles

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,7 +2,7 @@ import Controls from "../utils/Controls";
 
 export default function Home() {
   return (
-    <section className="py-14 bg-[#1B262C] text-white">
+    <section className="py-14 text-white">
       <div className="container px-5 mx-auto">
         <h1 className="text-center text-2xl md:text-4xl font-medium">
           Seeing the weather of the whole world <br /> with <span className="font-bold bg-gradient-to-r from-[#CAECFF] to-[#78CFFF] text-transparent bg-clip-text">Dark Weather!</span>
