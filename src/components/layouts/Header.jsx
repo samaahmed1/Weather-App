06b4d7f06e24963bbf167fe1d7f2f70a6e2c58eb
samaahmed1 <sama34777@gmail.com>
2025-03-04 20:38:08 +0200

    Add: ContactUs Pages

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -43,7 +43,7 @@ export default function Header() {
                 onClick={() => {
                   setOpenNav(false);
                 }}
-                to={"/"}
+                to={"/ContactUs"}
               >
                 Contact us
               </Link>
--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,18 @@ import App from "./App.jsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Header from "./components/layouts/Header.jsx";
+import ContactUs from "./components/pages/ContactUs.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import DownloadApp from "./components/pages/DownloadApp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/DownloadApp" element={<DownloadApp/>}/>
+        <Route path="/ContactUs" element={<ContactUs />} />
+        <Route path="/DownloadApp" element={<DownloadApp />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
